9a73e2978009b95a9e781d570f06bbcf64c87c59
Nikole Caballero Garrido <nikole@Nikoles-MacBook-Pro-15.local>
2024-01-29 19:54:26 -0400

    Microevaluación 2 - agregando servicios y dividiendo componentes

---
+++ src/app/components/pizza/pizza.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pizza',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pizza.component.html',
+  styleUrl: './pizza.component.scss'
+})
+export class PizzaComponent {
+  @Input() product : Product = {
+    id: "",
+    name: "",
+    price: 0,
+    img: ""
+  }
+
+  @Output() addedProduct = new EventEmitter<Product>();
+  onAddToCart(){
+    this.addedProduct.emit(this.product);
+  }
+}
--- src/app/components/popular-pizzas/popular-pizzas.component.ts
+++ src/app/components/popular-pizzas/popular-pizzas.component.ts
@@ -1,17 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { PizzaComponent } from '../pizza/pizza.component';
 
 @Component({
   selector: 'app-popular-pizzas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PizzaComponent],
   templateUrl: './popular-pizzas.component.html',
   styleUrl: './popular-pizzas.component.scss'
 })
 export class PopularPizzasComponent {
-  pizzas = [
-    { name: 'Pizza champiñones y jamón serrano', image: '../assets/images/p1.png' },
-    { name: 'Pizza margarita', image: '../assets/images/p5.png', link: 'pizza_margarita.html' },
-    { name: 'Pizza camarones', image: '../assets/images/p4.png' },
+  myShoppingCart: Product[] = [];
+  total = 0;
+
+  pizzas:Product[] = [
+    { id: '1', name: 'Pizza champiñones y jamón serrano', img: '../assets/images/p1.png', price: 60 },
+    { id: '2', name: 'Pizza margarita', img: '../assets/images/p5.png', link: 'pizza_margarita.html', price: 70 },
+    { id: '3', name: 'Pizza camarones', img: '../assets/images/p4.png', price: 80 },
   ];
+
+  onAddToShoppingCart(product: Product){
+    this.myShoppingCart.push(product)
+    console.log(this.myShoppingCart)
+    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    console.log(this.total)
+  }
 }
